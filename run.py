c1f8ecb7c4cfc49108ff447737875e27057010e8
Adam <me@adamburns.org>
2018-10-01 14:19:51 -0500

    Added definitions in main to run other modules

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
-import url_scrape.py
+import url_scrape.py as us
 
 
 def main():
+    for x in range(4):
+        open_url(modify_url(x))
+        export_links(open_output_file())
 
+
+def get_links():
+    for x in range(4):
+        open_url(modify_url(x))
+        export_links(open_output_file())
+
+
+def scrape_links():
+    
+    
 
 if __name__ == "__main__":
     main()
