8e42f55ed800cfee41774ea53d73c507f40a2924
Adam <me@adamburns.org>
2018-10-01 14:48:47 -0500

    Added close_output_file function and fixed string casting on startDoc

--- url_scrape.py
+++ url_scrape.py
@@ -2,11 +2,11 @@ import bs4 as bs
 import urllib.request
 
 #Constants
-URL_PREFIX = https://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text
-URL_SUFFIX = 
+URL_PREFIX = "https://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=*&sort=Date&startDoc="
+URL_SUFFIX = "&numResults=100&isAdv=true&formType=FormQUALIF&fromDate=mm/dd/yyyy&toDate=mm/dd/yyyy&stemming=true"
 
 
-def open_url():
+def open_url(URL):
     try: 
         broth = urllib.request.urlopen(URL).read()
     except (http.client.IncompleteRead) as e: 
@@ -16,7 +16,7 @@ def open_url():
 
 def modify_url(startDoc):
     URL = (URL_PREFIX
-           + startDoc
+           + str(startDoc)
            + URL_SUFFIX)
     return URL
 
@@ -30,10 +30,12 @@ def open_output_file():
     return output
 
 
-def export_links(output):
-    soup = bs.BeautifulSoup(broth, 'lxml')
-    for a in soup.find_all('a', href=True):
-        output.write(a['href'])
-        output.write("\n")
+def close_output_file(output):
     output.close()
-    
+
+
+def export_links(output, broth):
+    soup = bs.BeautifulSoup(broth, "lxml")
+    for a in soup.find_all("a", href=True):
+        output.write(a["href"])
+        output.write("\n")
